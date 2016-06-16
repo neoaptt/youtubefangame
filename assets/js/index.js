5c4ee88aa4d1f2842438ab48eb2f0113c6679af2
neoaptt <neoaptt@gmail.com>
2016-06-16 18:39:35 -0400

    progress bar

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,20 @@
 var avy = document.getElementById('youtuber-container').children;
+var emotionContainer = document.getElementById('youtuber-emotion-container');
+var happyBar = document.getElementById('happiness');
+var happiness = 50;
 for (var i = 0; i < avy.length; i++) {
     avy[i].onclick = selectAvy;
 }
+emotionContainer.onclick = emotionClick;
 function selectAvy() {
     for (var i = 0; i < avy.length; i++) {
         avy[i].className = '';
     }
     this.className = 'active';
+}
+function emotionClick() {
+    if (happiness <= 100) {
+        happiness++;
+    }
+    happyBar.style.width = happiness + "%"
 }
