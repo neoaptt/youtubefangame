13222b9eaa7699a781b40cdcfb18713508c29c50
neoaptt <neoaptt@gmail.com>
2016-06-16 09:21:44 -0400

    sidebar

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,10 @@
-
-
+var avy = document.getElementById('youtuber-container').children;
+for (var i = 0; i < avy.length; i++) {
+    avy[i].onclick = selectAvy;
+}
+function selectAvy() {
+    for (var i = 0; i < avy.length; i++) {
+        avy[i].className = '';
+    }
+    this.className = 'active';
+}
